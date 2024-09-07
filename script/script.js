490bfc574eb6a2def8684ced40fc3cb5e7892b7e
CindyKim0519 <bokyeong2018@gmail.com>
2024-09-07 17:17:47 +0900

    업데이트

---
+++ script/script.js
@@ -0,0 +1,60 @@
+$(function(){
+
+    $('ul.tabs li').click(function() {
+        var activeTab = $(this).attr('data-tab');
+        $('ul.tabs li').removeClass('on');
+        $('.tab_content').removeClass('on');
+        $(this).addClass('on');
+        $('#' + activeTab).addClass('on');
+});
+$('.list').slick({
+    dots: true,
+    nextArrow:'<i class="fa-solid fa-arrow-right right_arrow2">',
+    prevArrow:'<i class="fa-solid fa-arrow-left left_arrow2">'
+});
+$(".tabs").click(function(){
+    $('.list').slick('refresh');
+});   
+ 
+$('.tabs2 li').click(function() {
+    var activeTab = $(this).attr('data-tab');
+    $('.tabs2 li').removeClass('on');
+    $('.tab_content2').removeClass('on');
+    $(this).addClass('on');
+    $('#' + activeTab).addClass('on');
+});
+$('.list2').slick({
+    dots: true,
+    nextArrow:'<i class="fa-solid fa-arrow-right right_arrow3">',
+    prevArrow:'<i class="fa-solid fa-arrow-left left_arrow3">'
+});
+$(".tabs2").click(function(){
+    $('.list2').slick('refresh');
+});
+});
+
+$(window).on('load',function(){
+    setTimeout(function(){
+$('.post-slider').slick({
+dots: true,
+infinite: true,
+speed: 300,
+slidesToShow: 1,
+centerMode: true,
+variableWidth: true,
+nextArrow:'<i class="fa-solid fa-arrow-right right_arrow">',
+prevArrow:'<i class="fa-solid fa-arrow-left left_arrow">'
+});
+
+$('.post-slider2').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    nextArrow:'<i class="fa-solid fa-arrow-right right_arrow">',
+    prevArrow:'<i class="fa-solid fa-arrow-left left_arrow">'
+    });
+});
+});
